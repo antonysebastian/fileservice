f8ca9e67772ef9ab3b524c64309685497ac6592a
antonysebastian <antony.s.93@gmail.com>
2018-06-25 21:04:32 +0530

    Adding start/stop option in the start script to start/stop the service

--- src/start.sh
+++ src/start.sh
@@ -1,1 +1,35 @@
-nohup java -Djava.util.logging.manager=org.apache.logging.log4j.jul.LogManager -Dlog4j.configurationFile=./log-config.xml -jar file-service.jar > /dev/null 2>&1 &
+#!/bin/sh
+
+SERVICE="File Service"
+PIDFILE="./fileservice.pid"
+JAR_FILE="file-service.jar"
+OPTIONS="-Djava.util.logging.manager=org.apache.logging.log4j.jul.LogManager -Dlog4j.configurationFile=./log-config.xml"
+
+case $1 in
+    start)
+        echo "Starting $SERVICE server..."
+        if [ ! -f $PIDFILE ]; then
+            nohup java $OPTIONS -jar $JAR_FILE > /dev/null 2>&1 &
+            echo $! > $PIDFILE
+            echo "$SERVICE started"
+        else
+            echo "$SERVICE is running..."
+        fi
+    ;;
+
+    stop)
+        if [ -f $PIDFILE ]; then
+            PID=$(cat $PIDFILE);
+            echo "Stopping $SERVICE..."
+            kill $PID;
+            echo "$SERVICE stopped"
+            rm $PIDFILE
+        else
+            echo "$SERVICE is not running ..."
+        fi
+    ;;
+
+    *)
+        echo "Pass argument start/stop"
+    ;;
+esac
